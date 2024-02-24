26f7cfcd664a3dc6894304e3363d2d4c2373d950
jahval <59559335+jahval@users.noreply.github.com>
2024-02-24 21:55:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require('express');
 const app = express();
 
-const PORT = 8000;
+const port = 8000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
