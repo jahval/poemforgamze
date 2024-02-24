1f6d33ad78979a4c281826bda0265a5c5b018b8a
jahval <59559335+jahval@users.noreply.github.com>
2024-02-24 19:48:09 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 
 const port = 80;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
